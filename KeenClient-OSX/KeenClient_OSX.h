b87e2f7805d07761ba61f8234d5590ac44fad916
Rahul Jiresal <rahul.jiresal@gmail.com>
2014-02-04 09:01:21 -0800

    Initial Commit

---
+++ KeenClient-OSX/KeenClient_OSX.h
@@ -0,0 +1,13 @@
+//
+//  KeenClient_OSX.h
+//  KeenClient-OSX
+//
+//  Created by Rahul Jiresal on 2014-02-04.
+//  Copyright (c) 2014 Rahul Jiresal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KeenClient_OSX : NSObject
+
+@end
